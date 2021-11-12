6a439b53e91b277e4333797507aac76bec612ce7
Brandon Sorrell <81862313+BerlicTheHunter@users.noreply.github.com>
2021-11-12 09:30:50 -0500

    Update App.js
    changed unsplash collection

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App" style ={{backgroundImage: `url("https://source.unsplash.com/collection/4540043/1600x900")`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed',backgroundPosition: 'center' }}>
+    <div className="App" style ={{backgroundImage: `url("https://source.unsplash.com/collection/1061110/1600x900")`, backgroundRepeat: 'no-repeat', backgroundAttachment: 'fixed',backgroundPosition: 'center' }}>
       <div className="content" >
         <Navbar />
         <main className= "container" >
