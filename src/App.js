0e8766fb98264df5f3f0a40168d7e3822c5b7e9b
Berlic <bsorrell3@gmail.com>
2021-11-14 22:23:14 -0500

    added side nav, but buttons wont work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
-
+import 'materialize-css/dist/css/materialize.min.css';
 import Display from './components/Display';
+// import M from  'materialize-css/dist/js/materialize.min.js';
 
+// function componentDidMount() {
+//     let sidenav = document.querySelector('#slide-out');
+//     M.Sidenav.init(sidenav, {});
+//   }
 
 function App() {
+  
   return (
   <Display />
   )};
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import resume from '../Assets/Brandon Sorrell Resume Portfolio 081621.pdf';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from  'materialize-css/dist/js/materialize.min.js';
 
-
+// function componentDidMount() {
+//   let sidenav = document.querySelector('#slide-out');
+//   M.Sidenav.init(sidenav, {});
+// };
 
 export default function Navbar({ currentPage, handlePageChange }) {
    
-  
+  let sidenav = document.querySelector('#slide-out');
+  M.Sidenav.init(sidenav);
+
   return (
     <header className="navbar-fixed">
         <nav className= "light-blue accent-4">
             <div className="nav-wrapper">
-                <div className="brand-logo center">Brandon Sorrell</div>
+                <span className="brand-logo right">Brandon Sorrell</span>
+                <a href="no" data-target="slide-out" class="sidenav-trigger "><i class="material-icons">menu</i></a>
                 <ul id= "nav-mobile" className="left hide-on-med-and-down">
                     <li>
                       <a 
@@ -43,6 +51,38 @@ export default function Navbar({ currentPage, handlePageChange }) {
                 </ul>
             </div>
         </nav>
+        
+        <ul id="slide-out" class="sidenav">
+          <li>
+            <a 
+            href="#aboutmelink"
+            onClick={() => handlePageChange('AboutMe')} 
+            // className= "waves-effect btn-small green darken-1"
+            >
+              About Me
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#worklink"
+              onClick={() => handlePageChange('Work')} 
+              className= "waves-effect btn-small green darken-1"
+            >
+              Work
+            </a>
+          </li>
+          <li>
+            <a 
+              href="#contactForm" 
+              onClick={() => handlePageChange('ContactMe')} 
+              className= "waves-effect btn-small green darken-1"
+            >
+              Contact
+            </a>
+          </li>
+          <li><a href={resume} target="_blank" rel="noreferrer" className= "waves-effect btn-small green darken-1">Resume</a></li>
+        </ul>
+        
         </header>
   );
 }
